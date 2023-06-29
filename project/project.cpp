c28d5d2f8c04007cc8033218803a3689493a36ad
Alice(Nikita) <nikita@example.com>
2023-06-29 14:11:37 +0300

    code: another changes

--- project/project.cpp
+++ project/project.cpp
@@ -8,6 +8,8 @@ int main()
 	std::cout << "Enter A and B: ";
 	int a, b;
 	std::cin >> a >> b;
+	std::cout << "A + B = " << a + b << '\n'
+		<< "A - B = " << a - b << '\n';
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
